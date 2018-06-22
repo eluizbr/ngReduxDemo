eae09dac1e51c9c025b3f5b9eee2434ec7011a2a
Emerson Luiz <eluizbr@gmail.com>
2018-06-22 18:29:16 -0300

    Angular meetup

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+const CORE_MODULES = [
+  CommonModule,
+  ReactiveFormsModule,
+  HttpClientModule,
+  NgbModule
+];
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [CommonModule, ReactiveFormsModule, HttpClientModule, NgbModule],
+  imports: [CommonModule],
+  exports: [...CORE_MODULES],
   declarations: []
 })
-export class SharedModule {}
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    };
+  }
+}
